d007b660e9d062a32b8bea7f119db0d6953ba89b
hanthienduc <hanthienduc.96@gmail.com>
2022-07-27 20:34:35 +0900

    update navbar menu

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -11,10 +11,22 @@ import { useState } from 'react'
 
 function NavBar() {
     const [isShownDropDown, setIsShownDropDown] = useState(false)
+    const [noticeCount, setNoticeCount] = useState(1)
+    const [dropdownStyle, setDropDownStyle] = useState({
+        'display': 'none',
+        'height': '0px'
+    })
 
     function toggleDropDown(e) {
         setIsShownDropDown((prevState) => !prevState)
+        setDropDownStyle((prevStyle) => {
+            return {
+                'display': prevStyle.display === 'none' ? 'block' : 'none',
+                'height': prevStyle.height === '0px' ? 'auto' : '0px'
+            }
+        })
     }
+
     return (
         <header className='header'>
             <div className="logo">
@@ -24,17 +36,23 @@ function NavBar() {
                 <ul className="navbar-list">
                     <CustomListItem to={'myrecord'} src={icon_memo} text={'自分の記録'} />
                     <CustomListItem to={'/'} src={icon_challenge} text={'チャレンジ'} />
-                    <CustomListItem to={'/'} src={icon_info} text={'お知らせ'} />
+                    <div className='notice'>
+                        <CustomListItem to={'/'} src={icon_info} text={'お知らせ'} />
+                        <span className='notice-count'>{noticeCount}</span>
+                    </div>
                     <div className='dropdown-menu'>
-                        <img onClick={toggleDropDown} className='dropdown-menu-icon' src={isShownDropDown ? icon_close : icon_menu} alt={'menu'} />
-                        {isShownDropDown && <ul className='dropdown-list'  >
+                        <img id='dropdown-icon' onClick={toggleDropDown}
+                            className='dropdown-menu-icon'
+                            src={isShownDropDown ? icon_close : icon_menu}
+                            alt={'menu'} />
+                        <ul className='dropdown-list' style={dropdownStyle}  >
                             <li><Link to={'/'}>自分の記録</Link></li>
                             <li><Link to={'/'}>体重グラフ</Link></li>
                             <li><Link to={'/'}>目標</Link></li>
                             <li><Link to={'/'}>選択中のコース</Link></li>
                             <li><Link to={'/column'}>コラム一覧</Link></li>
                             <li><Link to={'/'}>設定</Link></li>
-                        </ul>}
+                        </ul>
                     </div>
                 </ul>
 
